7232f2bf6409dce116bdb8bf7c45b3161eeca37f
jinach0i <ginach0irk@gmail.com>
2024-01-12 13:06:56 +0900

    font 최적화

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Nanum_Myeongjo,Lusitana } from 'next/font/google';
+ 
+export const nanumMyeongjo = Nanum_Myeongjo({
+    subsets: ["latin"],
+    weight: ["400", "700", "800"]
+});
+ 
+export const lusitana  = Lusitana ({
+    subsets: ["latin"],
+    weight: ["400", "700"]
+});
