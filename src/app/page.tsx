f563897c34623490c6c55fe7a86345ed310962f6
jinach0i <ginach0irk@gmail.com>
2024-01-12 14:47:13 +0900

    Feat:Side메뉴Drawer 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
+import Link from "next/link";
 import React from "react";
-
 export default function Home() {
   return (
     <div id="wrap" className="h-screen" data-theme="cupcake">
@@ -16,9 +16,43 @@ export default function Home() {
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">환영합니다</h1>
             <p className="mb-5">
-             Framer Motion을 연습한 것을 기록 보관해둔 저장소입니다.
+              Framer Motion을 연습한 것을 기록 보관해둔 저장소입니다.
             </p>
-            <button className="btn btn-primary">목록</button>
+            <div className="drawer-content flex flex-col items-center justify-center">
+              <label
+                htmlFor="my-drawer-2"
+                className="btn btn-primary drawer-button lg:hidden"
+              >
+                목록
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="drawer lg:drawer-open">
+          <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-side">
+            <label
+              htmlFor="my-drawer-2"
+              aria-label="close sidebar"
+              className="drawer-overlay"
+            ></label>
+            <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+              <li>
+                <Link href="/scroll-chcr">문자 단위로 opacity나타나기</Link>
+              </li>
+              <li>
+                <Link href="/scroll-txt">문장 단위로 opacity나타나기</Link>
+              </li>
+              <li>
+                <Link href="/scroll-words">단어 단위로 opacity나타나기</Link>
+              </li>
+              <li>
+                <Link href="/scroll-chcr">문자 단위로 opacity나타나기</Link>
+              </li>
+              <li>
+                <Link href="/scroll-chcr">문자 단위로 opacity나타나기</Link>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
